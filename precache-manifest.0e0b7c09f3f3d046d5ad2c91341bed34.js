5cd5f7835b149e1b85eeadfffedf9a333815d744
Yann Gouffon <github@yago-art.ch>
2020-04-03 15:56:45 +0200

    Updates

---
+++ precache-manifest.0e0b7c09f3f3d046d5ad2c91341bed34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba8ce6d6ee0b5628bd7f025354830e9",
+    "url": "/react-srcset/index.html"
+  },
+  {
+    "revision": "4c37de9ed85bf0ddbb7f",
+    "url": "/react-srcset/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "9d901ad70f12bd310fda",
+    "url": "/react-srcset/static/js/2.1455618f.chunk.js"
+  },
+  {
+    "revision": "29397091d772b4b0b40d7845231d45ca",
+    "url": "/react-srcset/static/js/2.1455618f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c37de9ed85bf0ddbb7f",
+    "url": "/react-srcset/static/js/main.a8eaa0a5.chunk.js"
+  },
+  {
+    "revision": "ac9a9e84ccbbc52ca0b9",
+    "url": "/react-srcset/static/js/runtime-main.7e07b7b4.js"
+  }
+]);
